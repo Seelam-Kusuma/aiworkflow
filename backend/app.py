0a3838ca7a6d020fdddcc79365eb446582415a2b
Seelam-Kusuma <seelamkusumasri555@gmail.com>
2025-06-16 10:12:12 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  
+@app.route('/')
+def home():
+    return 'Flask backend is running!', 200
 
 @app.route('/sum', methods=['POST'])
 def calculate_sum():
